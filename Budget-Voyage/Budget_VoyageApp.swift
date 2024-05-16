be880b81c245d452361c6da4dea0e98a6e34ee50
Asif <md.mushfikhasanasif@gmail.com>
2024-05-16 14:01:40 -0400

    Initial Commit

---
+++ Budget-Voyage/Budget_VoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_VoyageApp.swift
+//  Budget-Voyage
+//
+//  Created by Asif on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Budget_VoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
